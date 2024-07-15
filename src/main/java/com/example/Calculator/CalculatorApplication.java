ec33911c8dc18ad184d3cbbce283a42809f1b22d
Aayu <119026312+AayushHans@users.noreply.github.com>
2024-07-15 15:00:36 +0530

    First Branch

---
+++ src/main/java/com/example/Calculator/CalculatorApplication.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CalculatorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+	@GetMapping("/calculator")
+	public String calculator(@RequestParam(value = "a", defaultValue = "0") int a,
+							@RequestParam(value = "b", defaultValue = "0") int b,
+							@RequestParam(value = "o", defaultValue = "+") String operation) {
+		int result;
+		switch (operation) {
+			case "-":
+				result = a - b;
+				break;
+			case "*":
+				result = a * b;
+				break;
+			case "/":
+				// Handle division by zero
+				if (b == 0) {
+					return "Error: Division by zero";
+				}
+				result = a / b;
+				break;
+			case "+":
+			default:
+				result = a + b;
+				break;
+		}
+		return String.format("%d", result);
+	}
+}
